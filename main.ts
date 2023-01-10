4a69fbca871a97a8063b2347d40452749b72e8c9
lina2405 <122355107+lina2405@users.noreply.github.com>
2023-01-10 16:58:16 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,56 @@
-input.onButtonPressed(Button.A, function () {
-    if (brightness < 0) {
-        brightness = 0
-    } else {
-        brightness += -16
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (brightness >= 255) {
-        brightness = 255
-    } else {
-        brightness += 16
-    }
-})
 input.onButtonPressed(Button.B, function () {
     if (colourSetting == 5) {
         colourSetting = 0
     } else {
         colourSetting += 1
     }
+    if (brightness >= 255) {
+        brightness = 255
+    } else {
+        brightness += 16
+    }
+    if (brightness == 0) {
+        brightness = 0
+    } else {
+        brightness += -16
+    }
 })
+let soilHue = 0
+let humidHue = 0
+let tempHue = 0
 let brightness = 0
 let colourSetting = 0
-let zipLEDs = kitronik_smart_greenhouse.createGreenhouseZIPDisplay(8)
-let zipStick = zipLEDs.zipStickRange()
+let zipLEDS = kitronik_smart_greenhouse.createGreenhouseZIPDisplay(8)
+let zipStick = zipLEDS.zipStickRange()
 colourSetting = 0
 brightness = 128
 zipStick.setBrightness(brightness)
+zipLEDS = kitronik_smart_greenhouse.createGreenhouseZIPDisplay(8)
+let statusLEDs = zipLEDS.statusLedsRange()
+basic.forever(function () {
+    basic.showNumber(kitronik_smart_greenhouse.temperature(TemperatureUnitList.C))
+    basic.showNumber(kitronik_smart_greenhouse.humidity())
+    basic.showNumber(kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1))
+})
+basic.forever(function () {
+    if (brightness >= 100) {
+        brightness = 0
+    }
+})
+basic.forever(function () {
+    if (brightness < 100) {
+        brightness = 120
+    }
+})
+basic.forever(function () {
+    tempHue = Math.map(kitronik_smart_greenhouse.temperature(TemperatureUnitList.C), 0, 40, 210, 0)
+    humidHue = Math.map(kitronik_smart_greenhouse.humidity(), 0, 100, 35, 150)
+    soilHue = Math.map(kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1), 0, 1023, 35, 150)
+    statusLEDs.setZipLedColor(0, kitronik_smart_greenhouse.hueToRGB(tempHue))
+    statusLEDs.setZipLedColor(1, kitronik_smart_greenhouse.hueToRGB(humidHue))
+    statusLEDs.setZipLedColor(2, kitronik_smart_greenhouse.hueToRGB(soilHue))
+    statusLEDs.show()
+})
 basic.forever(function () {
     zipStick.setBrightness(brightness)
     zipStick.show()
